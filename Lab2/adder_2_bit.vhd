f69ad9e872de6671d47a8577744deb375922ad3b
Donney Fan <donney.fan@hotmail.com>
2019-02-07 00:32:52 -0800

    Add subtractor functionality

--- Lab2/adder_2_bit.vhd
+++ Lab2/adder_2_bit.vhd
@@ -10,6 +10,7 @@ ENTITY adder_2_bit IS
 		-- Display/control
 		KEY0, KEY1, KEY2 : in STD_LOGIC;
 		DISPLAY: OUT std_logic_vector(0 to 1);
+		ADD_SUB_TOGGLE: in STD_LOGIC;
 		SWITCHES: IN std_logic_vector(0 to 1)
 	); 
 END;
@@ -18,7 +19,7 @@ ARCHITECTURE ripple_2_arch OF adder_2_bit IS
 
 	COMPONENT full_adder
 		PORT(
-			Cin, A, B: IN std_logic; 
+			Cin, A, B, M: IN std_logic; 
 			S, Cout: OUT std_logic
 			);
 	END COMPONENT;
@@ -30,17 +31,17 @@ ARCHITECTURE ripple_2_arch OF adder_2_bit IS
 
 	BEGIN
 		-- Create two full adders and map their pins.
-		FA1: full_adder PORT MAP ('0', A(0), B(0), S(0), t1);
-		FA2: full_adder PORT MAP (t1, A(1), B(1), S(1), Cout);
+		FA1: full_adder PORT MAP (ADD_SUB_TOGGLE, A(0), B(0), ADD_SUB_TOGGLE, S(0), t1);
+		FA2: full_adder PORT MAP (t1, A(1), B(1), ADD_SUB_TOGGLE, S(1), Cout);
 		
-	PROCESS (KEY0, KEY1, KEY2)
-    begin
+	PROCESS (KEY0, KEY1, KEY2, ADD_SUB_TOGGLE)
+	begin
 		-- Assign the data conditionally. (Implicit latch)
         if KEY0 = '0' then
 				A <= SWITCHES;
         elsif KEY1 = '0' then
             B <= SWITCHES;
-		  elsif KEY2 = '0' then
+		  elsif KEY2 = '0'then
 				DISPLAY <= S;
         else
             DISPLAY <= SWITCHES;
--- Lab2/full_adder.vhd
+++ Lab2/full_adder.vhd
@@ -1,9 +1,10 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
+use ieee.std_logic_unsigned.all;
 
 ENTITY full_adder IS
 	PORT (
-		Cin, A, B: IN std_logic;
+		Cin, A, B, M: IN std_logic;
 		S, Cout: OUT std_logic
 	); 
 END;
@@ -11,9 +12,8 @@ END;
 ARCHITECTURE main OF full_adder IS
 
 	BEGIN
-	
-	S <= Cin XOR A XOR B;
-	
+		
+	S <= Cin XOR A XOR (B XOR M);
 	Cout <= (A AND B) OR (Cin AND B) OR (Cin AND A);
 	
 END main;
