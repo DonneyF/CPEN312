ac06894e4a825b193b2d93464934cc8bccd50e4f
Donney Fan <donney.fan@hotmail.com>
2019-02-12 10:59:41 -0800

    Display output to all 6 displays

--- BCDAdderSubtractor/BCDAdderSubtractor.vhd
+++ BCDAdderSubtractor/BCDAdderSubtractor.vhd
@@ -7,7 +7,7 @@ ENTITY BCDadderSubtractor IS
 		-- Display/control
 		KEY0, KEY1, KEY2 : in STD_LOGIC;
 		--DISPLAY: OUT std_logic_vector(7 downto 0);
-		SevSeg1, SevSeg2: OUT std_logic_vector(0 to 6);
+		SevSeg0, SevSeg1, SevSeg2, SevSeg3, SevSeg4, SevSeg5: OUT std_logic_vector(0 to 6);
 		ADD_SUB_TOGGLE: in STD_LOGIC;
 		OVERFLOW: OUT STD_LOGIC;
 		SWITCHES: IN std_logic_vector(7 downto 0)
@@ -17,7 +17,7 @@ END;
 ARCHITECTURE main OF BCDadderSubtractor IS
 	
 	-- Temporary carry signal
-	SIGNAL carry: std_logic_vector(1 downto 0) := "00";
+	SIGNAL carry: std_logic_vector(1 downto 0);
 	-- Signals for values
 	SIGNAL A, B: std_logic_vector(7 downto 0);
 	SIGNAL S: std_logic_vector(7 downto 0);
@@ -45,23 +45,32 @@ BEGIN
 	BCDAS_1: bcd_addsub PORT MAP(A(3 downto 0), B(3 downto 0), ADD_SUB_TOGGLE, ADD_SUB_TOGGLE, S(3 downto 0), carry(0));
 	BCDAS_2: bcd_addsub PORT MAP(A(7 downto 4), B(7 downto 4), ADD_SUB_TOGGLE, carry(0), S(7 downto 4), carry(1));
 	
-	OVERFLOW <= CARRY(1);
+	ADisp0: BCDto7Seg PORT MAP(A(3 downto 0), SevSeg4);
+	ADisp1: BCDto7Seg PORT MAP(A(7 downto 4), SevSeg5);
 	
-	BCDDisplay1: BCDto7Seg PORT MAP(OUTVAL(3 downto 0), SevSeg1);
-	BCDDisplay2: BCDto7Seg PORT MAP(OUTVAL(7 downto 4), SevSeg2);
+	BDisp0: BCDto7Seg PORT MAP(B(3 downto 0), SevSeg2);
+	BDisp1: BCDto7Seg PORT MAP(B(7 downto 4), SevSeg3);
 	
-	PROCESS (KEY0, KEY1, KEY2)
+	
+	SumDisp0: BCDto7Seg PORT MAP(OUTVAL(3 downto 0), SevSeg0);
+	SumDisp1: BCDto7Seg PORT MAP(OUTVAL(7 downto 4), SevSeg1);
+	
+	PROCESS (KEY0, KEY1, KEY2, ADD_SUB_TOGGLE)
 	begin
 		-- Assign the data conditionally. (Implicit latch)
 		if KEY1 = '0' then
 			A <= SWITCHES;
 		elsif KEY0 = '0' then
 			B <= SWITCHES;
-		elsif KEY2 = '0'then
-			OUTVAL <= S;
 		else
-			OUTVAL <= SWITCHES;
+			OUTVAL <= S;
 		end if;
+		
+		IF ADD_SUB_TOGGLE = '0' THEN
+			OVERFLOW <= CARRY(1);
+		ELSE
+			OVERFLOW <= '0';
+		END IF;
 	 end process;
 	
 END main;
