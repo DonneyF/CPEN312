066290b1f766ed33935e57ed4352503f942bc391
Donney Fan <donney.fan@hotmail.com>
2019-03-09 01:22:30 -0800

    Finish main portion. Need to re-factor

--- Lab4.asm
+++ Lab4.asm
@@ -36,7 +36,6 @@ init:
 main:
 	; Compare switch with 000
 	mov A, SWA
-	;cjne A, #00000001H, NOT_001
 	cjne A, #00000000H, NOT_000
 	; If equal, display the six most sigificant digits
 	mov HEX5, #DIGIT_2
@@ -64,7 +63,7 @@ NOT_000:
 	ljmp LATCH_SW3
 
 NOT_001:
-	cjne A, #00000002H, wait_half_second
+	cjne A, #00000002H, NOT_002
 	; Starting with the six most significant digits, scroll one digit to the left every second
 	; Get out
 	jb KEY.3, LATCH_SW3
@@ -102,25 +101,157 @@ SCROLL_LEFT:
 	; Wait a full second
 	lcall wait_half_second
 	lcall wait_half_second
-
 	; Exit condition
 	jnb KEY.3, LATCH_SW3
 	ljmp SCROLL_LEFT
 
-;dont_do_this:
+; Some aliases due to jump distance
+wait_half_second_alias1: ljmp wait_half_second
+LATCH_SW3_ALIAS: ljmp LATCH_SW3
 
-;	; Check if SWA is 01010100.  If so display "------"
-;	mov a, swa
-;	cjne a, #01010100b, not_01010100
-;	; Display "------"		
-;	mov HEX4, #DASH
-;	mov HEX3, #DASH
-;	mov HEX2, #DASH
-;	mov HEX1, #DASH
-;	mov HEX0, #DASH
-;	lcall wait
-;not_01010100:
+NOT_002:
+	cjne A, #00000003H, NOT_003
+	; Starting with the six most significant digits, scroll one digit to the right every second
+	; Get out
+	jb KEY.3, LATCH_SW3
+	; Initialize the displays
+	mov HEX5, #DIGIT_2
+	mov HEX4, #DIGIT_1
+	mov HEX3, #DIGIT_3
+	mov HEX2, #DIGIT_1
+	mov HEX1, #DIGIT_6
+	mov HEX0, #DIGIT_1
+	mov R5, #DIGIT_6
+	mov R4, #DIGIT_1
+	; Need an extra register for to not overwrite data
+	mov R3, #DIGIT_2
+	lcall wait_half_second_alias1
+	ljmp SCROLL_RIGHT
+
+SCROLL_RIGHT:
+	; Scroll left, so the value in HEX5 goes to HEX4
+	mov A, R4
+	mov R3, A
+
+	mov A, R5
+	mov R4, A
+
+	mov R5, HEX0
+	mov HEX0, HEX1
+	mov HEX1, HEX2
+	mov HEX2, HEX3
+	mov HEX3, HEX4
+	mov HEX4, HEX5
+	mov HEX5, R3
+	; Wait a full second
+	lcall wait_half_second_alias1
+	lcall wait_half_second_alias1
+	; Exit condition
+	jnb KEY.3, LATCH_SW3_ALIAS
+	ljmp SCROLL_RIGHT
+
+NOT_003:
+	;Make the six least significant digits blink every second
+	cjne A, #00000004H, NOT_004
+	mov HEX5, #DIGIT_3
+	mov HEX4, #DIGIT_1
+	mov HEX3, #DIGIT_6
+	mov HEX2, #DIGIT_1
+	mov HEX1, #DIGIT_6
+	mov HEX0, #DIGIT_1
+
+	lcall wait_half_second_alias1
+	lcall wait_half_second_alias1
+
+	mov HEX5, #BLANK
+	mov HEX4, #BLANK
+	mov HEX3, #BLANK
+	mov HEX2, #BLANK
+	mov HEX1, #BLANK
+	mov HEX0, #BLANK
+
+	lcall wait_half_second_alias1
+	lcall wait_half_second_alias1
+
+	jnb KEY.3, LATCH_SW3_ALIAS
+	jmp NOT_003
+
+; Some aliases due to jump distance
+wait_half_second_alias2: ljmp wait_half_second
+LATCH_SW3_ALIAS2: ljmp LATCH_SW3
+
+NOT_004:
+	cjne A, #00000005H, NOT_005
+	;Make the six most significant digits appear one at time every second, starting with a blank display
+	;Init blank
+	mov HEX5, #BLANK
+	mov HEX4, #BLANK
+	mov HEX3, #BLANK
+	mov HEX2, #BLANK
+	mov HEX1, #BLANK
+	mov HEX0, #BLANK
+
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+
+	mov HEX5, #DIGIT_2
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+	mov HEX4, #DIGIT_1
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+	mov HEX3, #DIGIT_3
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+	mov HEX2, #DIGIT_1
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+	mov HEX1, #DIGIT_6
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+	mov HEX0, #DIGIT_1
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+
+	jnb KEY.3, LATCH_SW3_ALIAS2
+	jmp NOT_004
+
+LATCH_SW3_ALIAS3: ljmp LATCH_SW3
+
+NOT_005:
+	cjne A, #00000006H, wait_half_second_alias2
+
+	mov HEX5, #09H ; H
+	mov HEX4, #06H; E
+	mov HEX3, #47H; L
+	mov HEX2, #47H; L
+	mov HEX1, #40H; 0
+	mov HEX0, #BLANK
+
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+
+	mov HEX5, #DIGIT_2
+	mov HEX4, #DIGIT_1
+	mov HEX3, #DIGIT_3
+	mov HEX2, #DIGIT_1
+	mov HEX1, #DIGIT_6
+	mov HEX0, #DIGIT_1
+
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+
+	mov HEX5, #46H ; C
+	mov HEX4, #0CH; P
+	mov HEX3, #48H; N
+	mov HEX2, #30H; 3
+	mov HEX1, #79H; 1
+	mov HEX0, #24H ;2
 	
-    sjmp main
+	lcall wait_half_second_alias2
+	lcall wait_half_second_alias2
+
+	jnb KEY.3, LATCH_SW3_ALIAS3
+	jmp NOT_005
 
 END
